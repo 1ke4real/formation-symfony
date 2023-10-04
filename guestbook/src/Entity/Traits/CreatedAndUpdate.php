861318fa071407f007160db2507761822f5073f2
Mike Leman <mikeeleman@gmail.com>
2023-10-04 14:40:36 +0200

    clean dump

--- guestbook/src/Entity/Traits/CreatedAndUpdate.php
+++ guestbook/src/Entity/Traits/CreatedAndUpdate.php
@@ -6,21 +6,18 @@ trait CreatedAndUpdate {
 
     public function getUpdatedAt(): ?\DateTimeImmutable
     {
-        dump($this->updatedAt);
         return $this->updatedAt;
 
     }
 
     public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
     {
-        dump($this->updatedAt);
         $this->updatedAt = $updatedAt;
         return $this;
     }
 
     public function getCreatedAt(): ?\DateTimeImmutable
     {
-        dump($this->createdAt);
         return $this->createdAt;
     }
 
