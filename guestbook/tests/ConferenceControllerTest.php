b15cac4225b88566d522572a5264f1e6b572ee61
Mike Leman <mikeeleman@gmail.com>
2023-10-05 11:57:47 +0200

    test crawler

--- guestbook/tests/ConferenceControllerTest.php
+++ guestbook/tests/ConferenceControllerTest.php
@@ -12,28 +12,33 @@ class ConferenceControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Give your feedback!');
+        $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
 
-    public function testConferencePage(){
+    public function testConferencePage()
+    {
         $client = static::createClient();
         $crawler = $client->request('GET', '/conference/amsterdam-2019');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertCount(2, $crawler->filter('p:contains("There are 1 comments")'));
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019 Conférences');
+        $this->assertSelectorTextContains('h3', 'Add your feedback');
+        $this->assertSelectorTextContains('p', 'This was a great conference.');
+
     }
 
-    public function testCommentSubmission(){
+    public function testCommentSubmission()
+    {
         $client = static::createClient();
         $client->request('GET', '/conference/amsterdam-2019');
         $client->submitForm('Submit', [
             'comment_form[author]' => 'Fabien',
             'comment_form[text]' => 'Some feedback from an automated functional test',
-            'comment_form[email]' => $email = 'me@automat.ed',
-            'comment_form[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
         ]);
         $this->assertResponseRedirects();
         $client->followRedirect();
         $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
 }
